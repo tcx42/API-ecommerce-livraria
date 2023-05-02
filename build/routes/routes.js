1ad7fa4d061f070e6665b3a277a36808a92b0c94
Tiago Xavier <tiagocorx@gmail.com>
2023-05-02 14:39:00 -0300

    fix: remove build directory

--- build/routes/routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const routes = (0, express_1.Router)();
-routes.get("/", (req, res) => {
-    res.status(200).json("ok");
-});
-exports.default = routes;
