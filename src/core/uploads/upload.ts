1c7c4c1322fe88c19953fcb093bf7d0c4fd9938c
Tiago Xavier <tiagocorx@gmail.com>
2023-06-06 19:50:25 -0300

    fix: image extention

--- src/core/uploads/upload.ts
+++ src/core/uploads/upload.ts
@@ -6,7 +6,8 @@ const storage = multer.diskStorage({
     cb(null, path.resolve("images"));
   },
   filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9) +
+      file.originalname;
     cb(null, uniqueSuffix);
   },
 });
